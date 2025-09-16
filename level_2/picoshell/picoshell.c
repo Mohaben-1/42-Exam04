0ea345c9cdabfefb4fe4b6c34d5f6ad721ccec8f
Mohaben-1 <mohammedwhb2001@gmail.com>
2025-09-16 16:00:45 +0100

    Exam Rank 04

--- level_2/picoshell/picoshell.c
+++ level_2/picoshell/picoshell.c
@@ -7,12 +7,9 @@ int	picoshell(char **cmds[])
 	pid_t	pid;
 	int		pipefd[2];
 	int		prev_fd;
-	int		status;
-	int		exit_code;
 	int		i;
 
 	prev_fd = -1;
-	exit_code = 0;
 	i = 0;
 	while (cmds[i])
 	{
@@ -55,65 +52,6 @@ int	picoshell(char **cmds[])
 		}
 		i++;
 	}
-	while (wait(&status) > 0)
-	{
-		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-			exit_code = 1;
-	}
-	return (exit_code);
+	while (wait(NULL) > 0);
+	return (0);
 }
-
-
-/* Test main
-
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-static int	count_cmds(int argc, char **argv)
-{
-	int	count = 1;
-	for (int i = 1; i < argc; i++)
-	{
-		if (strcmp(argv[i], "|") == 0)
-			count++;
-	}
-	return (count);
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc < 2)
-		return (fprintf(stderr, "Usage: %s cmd1 [args] | cmd2 [args] ...\n", argv[0]), 1);
-
-	int	cmd_count = count_cmds(argc, argv);
-	char	***cmds = calloc(cmd_count + 1, sizeof(char **));
-	if (!cmds)
-		return (perror("calloc"), 1);
-
-	int	i = 1, j = 0;
-	while (i < argc)
-	{
-		int	len = 0;
-		while (i + len < argc && strcmp(argv[i + len], "|") != 0)
-			len++;
-		cmds[j] = calloc(len + 1, sizeof(char *));
-		if (!cmds[j])
-			return (perror("calloc"), 1);
-		for (int k = 0; k < len; k++)
-			cmds[j][k] = argv[i + k];
-		cmds[j][len] = NULL;
-		i += len + 1;
-		j++;
-	}
-	cmds[cmd_count] = NULL;
-
-	int	ret = picoshell(cmds);
-
-	// Clean up
-	for (int i = 0; cmds[i]; i++)
-		free(cmds[i]);
-	free(cmds);
-
-	return (ret);
-}
-*/
